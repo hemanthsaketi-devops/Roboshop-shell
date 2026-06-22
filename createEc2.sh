a5092c5cdf58f18f37ef028c2b1e76ee0b855a76
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-06-22 16:07:07 +0530

    create ec2

--- createEc2.sh
+++ createEc2.sh
@@ -1,89 +1,47 @@
 #!/bin/bash
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-
 domain_name="daws93s.online"
 hosted_zone_id="Z02671512D0FTHVVKN2M"
 
-ami_id="ami-0220d79f3f480ecf5"
-security_group_id="sg-0fea5e49e962e81c9"
-subnet_id="subnet-0686896146c8f390b"
+for name in ${instances[@]}; do
+    if [ $name == "shipping" ] || [ $name == "mysql" ]
+    then
+        instance_type="t3.medium"
+    else
+        instance_type="t3.micro"
+    fi
+    echo "Creating instance for: $name with instance type: $instance_type"
+    instance_id=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type $instance_type --security-group-ids sg-0fea5e49e962e81c9 --subnet-id subnet-09863c54177764565 --query 'Instances[0].InstanceId' --output text)
+    echo "Instance created for: $name"
 
-for name in "${instances[@]}"
-do
-if [[ "$name" == "shipping" || "$name" == "mysql" ]]
-then
-instance_type="t3.medium"
-else
-instance_type="t3.micro"
-fi
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
 
-```
-echo "====================================================="
-echo "Creating instance for: $name"
-echo "Instance Type: $instance_type"
-echo "====================================================="
+    if [ $name == "web" ]
+    then
+        aws ec2 wait instance-running --instance-ids $instance_id
+        public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
+        ip_to_use=$public_ip
+    else
+        private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+        ip_to_use=$private_ip
+    fi
 
-instance_id=$(aws ec2 run-instances \
-    --image-id "$ami_id" \
-    --instance-type "$instance_type" \
-    --security-group-ids "$security_group_id" \
-    --subnet-id "$subnet_id" \
-    --query 'Instances[0].InstanceId' \
-    --output text)
-
-if [ -z "$instance_id" ] || [ "$instance_id" == "None" ]
-then
-    echo "Failed to create instance for $name"
-    continue
-fi
-
-echo "Instance Created: $instance_id"
-
-aws ec2 create-tags \
-    --resources "$instance_id" \
-    --tags Key=Name,Value="$name"
-
-aws ec2 wait instance-running \
-    --instance-ids "$instance_id"
-
-if [ "$name" == "web" ]
-then
-    ip_to_use=$(aws ec2 describe-instances \
-        --instance-ids "$instance_id" \
-        --query 'Reservations[0].Instances[0].PublicIpAddress' \
-        --output text)
-else
-    ip_to_use=$(aws ec2 describe-instances \
-        --instance-ids "$instance_id" \
-        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-        --output text)
-fi
-
-echo "IP Address: $ip_to_use"
-
-echo "Creating Route53 record for $name"
-
-aws route53 change-resource-record-sets \
-    --hosted-zone-id "$hosted_zone_id" \
-    --change-batch "{
-        \"Comment\": \"Creating record for $name\",
-        \"Changes\": [{
-            \"Action\": \"UPSERT\",
-            \"ResourceRecordSet\": {
-                \"Name\": \"$name.$domain_name\",
-                \"Type\": \"A\",
-                \"TTL\": 1,
-                \"ResourceRecords\": [{
-                    \"Value\": \"$ip_to_use\"
-                }]
-            }
+    echo "creating R53 record for $name"
+    aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
+    {
+        "Comment": "Creating a record set for '$name'"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$name.$domain_name'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$ip_to_use'"
+            }]
+        }
         }]
-    }"
-
-echo "$name Route53 record created"
-```
-
+    }'
+    
 done
-
-echo "All instances created successfully"
